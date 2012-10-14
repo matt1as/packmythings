51e2cbf66001d601fd6cb1db415f9ac47068def7
Mattias Johansson <mattias.e.johansson@gmail.com>
2012-10-14 22:02:09 +0200

    Moved SITE_ID to local_settings.py

--- packmythings/settings.py
+++ packmythings/settings.py
@@ -51,7 +51,6 @@ TIME_ZONE = 'Europe/Stockholm'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 4
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
