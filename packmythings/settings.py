4b7d03b9c6ca5eaa916abe6ac1a1dd611fcfd795
Mattias Johansson <mattias.e.johansson@gmail.com>
2012-10-14 16:47:22 +0200

    Moved additional local settings to local_settings.py

--- packmythings/settings.py
+++ packmythings/settings.py
@@ -43,32 +43,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -114,11 +88,6 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
-EMAIL_HOST = 'pop.gmail.com'
-EMAIL_HOST_PASSWORD = 'Nischkima31PaMM'
-EMAIL_HOST_USER = 'mattias.e.johansson@gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
 
 INSTALLED_APPS = (
     'django.contrib.auth',
